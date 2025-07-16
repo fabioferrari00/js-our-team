f5fd1563d3d0cf33bcdeb41b7e6ea7e091bb5acf
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-16 13:42:25 +0200

    [upd] ciclo l'array di oggetti e lo inserisco nell'html

--- js/script.js
+++ js/script.js
@@ -36,3 +36,39 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+//definizione della fuinzione che mi genera la struttura delle card
+const generateCard = (member) => {
+
+  const { name, role, email, img } = member;
+
+  //mi creo una variabile che conterrà l'html e le informazioni della card
+  const memberCard = `<div class="col-12 col-md-4">
+        <div class="card mb-3 bg-black">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="./assets/${img}" class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title text-light" id="name">${name}</h5>
+                <p class="card-text text-light" id="role">${role}</p>
+                <p class="card-text text-primary" id="email">${email}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+  return memberCard;
+
+}
+
+const teamContainer = document.getElementById('team-members');
+
+for (let i = 0; i < teamMembers.length; i++) {
+
+  let card = generateCard(teamMembers[i]);
+
+  teamContainer.innerHTML += card;
+
+}
